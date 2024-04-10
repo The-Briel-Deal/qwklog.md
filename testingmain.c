1493d97bfd43770760cc003497425378e38f93f1
Gabriel Ford <gabe@fordltc.net>
2024-04-09 23:28:34 -0400

    More Changes to try to stop the wonkiness

--- testingmain.c
+++ testingmain.c
@@ -4,23 +4,24 @@
 
 void addlb(char *str) {
   // Allocate the length of the string passed in + 2 for linebreak and null byte
-  str = (char *)realloc(str, strlen(str) + 2);
+  str = (char *)realloc(str, strlen(str) + 3);
   // Create linebreak in memory
-  char *lb = (char *)malloc(2);
+  char *lb = (char *)malloc(3);
   strcpy(lb, "\n");
   // Concatenate the linebreak onto the string
   strcat(str, lb);
+  free(lb);
 }
 
 void addspace(char *str) {
-  char *space = " ";
   // Allocate the length of the string passed in + 2 for linebreak and null byte
-  str = (char *)realloc(str, strlen(str) + 2);
+  str = (char *)realloc(str, strlen(str) + 3);
   // Create linebreak in memory
-  char *lb = (char *)malloc(2);
-  strcpy(lb, space);
+  char *space = (char *)malloc(3);
+  strcpy(space, " ");
   // Concatenate the linebreak onto the string
-  strcat(str, lb);
+  strcat(str, space);
+  free(space);
 }
 
 void addarg(char *linetowrite, char *nextarg) {
@@ -35,28 +36,29 @@ int main(int argc, char **argv) {
   // Create a pointer of type FILE to the textfile filemode.txt
   FILE *fptr;
   fptr = fopen("test_log.txt", "a");
-  // If file doesn't exist (pointer is null) return
-  if (fptr == NULL) {
-    return 1;
-  }
   // Create Line that will be appended to and written at end
-  char *linetowrite = (char *)malloc(1);
-  strcpy(linetowrite, "");
+  char *linetowrite;
+  linetowrite = (char *)malloc(7);
   // Iterate through all args
   for (int i = 0; i < argc; i++) {
     // Allocate Memory of length arg + 1 so that there is room for null byte
-    char *argcopy = (char *)malloc(strlen(argv[i]) + 1);
+    char *argcopy = (char *)malloc(strlen(argv[i]) + 10);
     // Copy the sys arg to a copy as the sys arg can't be mutated without
     // undefined behavior
     strcpy(argcopy, argv[i]);
+    printf("argis: %s\n", argcopy);
     // Append this arg onto linetowrite
     addarg(linetowrite, argcopy);
+    free(argcopy);
+    printf("line after addarg: %s\n", linetowrite);
     // Add space onto linetowrite
     addspace(linetowrite);
+    printf("line at end of loop: %s\n", linetowrite);
   }
   // Add linebreak using function defined above
   addlb(linetowrite);
   // Write this arg to file
+  printf("%s", linetowrite);
   fprintf(fptr, "%s", linetowrite);
   // Free the files pointer in memory
   fclose(fptr);
